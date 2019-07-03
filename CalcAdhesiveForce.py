75ff8e5bffa1ca280535f689550a452201ab782e
Takayuki Kameoka <T.kameoka10032@gmail.com>
2019-07-03 14:04:02 +0900

    通常のリストを使って複数ファイルの読み込みに成功

--- CalcAdhesiveForce.py
+++ CalcAdhesiveForce.py
@@ -26,82 +26,93 @@ def ClickRefButton():
     filepath = filedialog.askopenfilenames(filetypes = fTyp, initialdir = iDir)# ファイルダイアログを表示し，複数ファイルを選択
     # file1.set(filepath)# file1に絶対パスをセット
     # # ファイルリストを作成，参照できるようにグローバル変数にする
-    # global file_list
-    # file_list = list(filepath)
+    global file_list
+    file_list = list(filepath)
     # ファイルリストを作成，setを使ってみる
-    file_list.set(filepath)
+    # file_list.set(filepath)
     # getに対して要素を指定できるか
     # print(file1.get())
     # set, getは辞書型{'key1': 'val1', 'key2': 'val2', 'key3': 'val3'}
     # 数字の指定で要素を抜き出せるか
-    print(file_list.get(2))
+    # print(file_list.get(2))
     
 
 # Figureボタンを押したときの動作
 def ClickedFigureButton ():
     # 読み込んだファイルリストのパスをGenerateFigures()に渡していく
-    print("ClickedFigureButton")
-    # print(file_list)
-    # ファイルパス引き渡しテスト
-    GenerateFigures(file_list.get())
-
+    for path in file_list:
+        GenerateFigures(path)
+    
+    # 計測が終わったら終了メッセージを表示
+    messagebox.showinfo("status", "グラフの生成が終わりました．")
 
 
 # 粘着力の計算とグラフの生成，保存
-# 後で別関数に分けたほうがいいかも
-# CSVファイルパスを引数にしたほうがいい？（.set()，.get()を理解しきれていない）
 def GenerateFigures(_path):
 
-    print(_path)
-    print(file_list.get())
+    # print("_Path")
+    # print(_path)
 
     # # csv_file_name = "20190626194837"
     # # ファイル名と拡張子を取得する
     # # csv_file_name, ext = os.path.splitext(os.path.basename(file1.get()))
-    # # csv_file_name, ext = os.path.splitext(os.path.basename(str(_path))
+    csv_file_name, ext = os.path.splitext(os.path.basename(_path))
     # csv_file_name, ext = os.path.splitext(os.path.basename("./sample_data_20190626194837.csv")
 
     # # # 参照ファイル（file1）を開く
     # # csv_input = pd.read_csv(filepath_or_buffer=  file1.get(),
     # #                     encoding = "utf_8", sep=",", engine="python",
     # #                     header = None)
-    # # 引数ファイルパスを開く
-    # csv_input = pd.read_csv(filepath_or_buffer = _path,
-    #                     encoding = "utf_8", sep=",", engine="python",
-    #                     header = None)
+    # 引数ファイルパスを開く
+    csv_input = pd.read_csv(filepath_or_buffer = _path,
+                        encoding = "utf_8", sep=",", engine="python",
+                        header = None)
 
-    # # グラフ画像を保存するディレクトリの作成（参照ファイルと同一階層内に作る）
-    # # figure_dir_path = "./" + csv_file_name + "_figures"
-    # figure_dir_path = os.path.dirname(csv_file_path) + "/" + csv_file_name + "_figures"
-    # os.mkdir(figure_dir_path)
+    # グラフ画像を保存するディレクトリの作成（参照ファイルと同一階層内に作る）
+    # figure_dir_path = "./" + csv_file_name + "_figures"
+    figure_dir_path = os.path.dirname(_path) + "/" + csv_file_name + "_figures"
+    os.mkdir(figure_dir_path)
 
-    # #オフセット行を格納
-    # offset = []
-    # offset = csv_input.iloc[1,:]
+    #オフセット行を格納
+    offset = []
+    offset = csv_input.iloc[1,:]
 
-    # #lenで行数を取得
-    # for i in range(len(csv_input)):
-    # #10行まででテスト
-    # # for i in range(10):
-    # #    print(csv_input.iloc[i,:]-offset)
-    #     #オフセット値から各計測地を減算して表示
-    #     plt.plot(offset - csv_input.iloc[i,:], marker = "o")
-    #     plt.ylim([-5,40])#Y軸の幅を指定
-    #     plt.title(i)#タイトルをフレーム数にする
-    #     plt.xlabel("X-axis")#x軸の名前．
-    #     plt.ylabel("Y-axis")#y軸の名前．あとで力（mN）にする
-    #     file_name ="{0:03d}".format(i) + '.png' #3桁の連版の名前をつける
-    #     plt.savefig(figure_dir_path + "/" + file_name)
-    #     plt.figure()
+    #lenで行数を取得
+    for i in range(len(csv_input)):
+    #10行まででテスト
+    # for i in range(10):
+    #    print(csv_input.iloc[i,:]-offset)
+        #オフセット値から各計測地を減算して表示
+        plt.plot(offset - csv_input.iloc[i,:], marker = "o")
+        plt.ylim([-5,40])#Y軸の幅を指定
+        plt.title(i)#タイトルをフレーム数にする
+        plt.xlabel("X-axis")#x軸の名前．
+        plt.ylabel("Y-axis")#y軸の名前．あとで力（mN）にする
+        file_name ="{0:03d}".format(i) + '.png' #3桁の連版の名前をつける
+        plt.savefig(figure_dir_path + "/" + file_name)
+        plt.figure()
+
+
+# Peakボタンを押したときの動作
+def ClickedPeakButton ():
+    # 計算したピーク値とそのフレーム番号を入れるリスト
+    peak_list = []
+    # 読み込んだファイルリストのパスをPeakAdhesiveForce()に渡していく
+    for path in file_list:
+        # print(PeakAdhesiveForce (path))
+        # 要素を追加
+        peak_list.append( PeakAdhesiveForce (path) )
+    # ピーク値とそのフレームを表示
+    print(peak_list)
 
     # 計測が終わったら終了メッセージを表示
-    messagebox.showinfo("status", "グラフの生成が終わりました．")
+    messagebox.showinfo("status", "ピーク値の計算が終わりました．")
 
 
 # 粘着力のピーク値を計算する
-def PeakAdhesiveForce():
+def PeakAdhesiveForce(_path):
     # 参照ファイル（file1）を開く
-    csv_input = pd.read_csv(filepath_or_buffer=  file_list.get(),
+    csv_input = pd.read_csv(filepath_or_buffer=  _path,
                         encoding = "utf_8", sep=",", engine="python",
                         header = None)
     
@@ -129,9 +140,11 @@ def PeakAdhesiveForce():
             
     # ピーク値の表示
     # print(peak_force)
+    # ピーク値を返す
+    return (peak_force, peak_flame)
 
     # 計測が終わったら終了メッセージを表示
-    messagebox.showinfo("status", "ピーク粘着力の計算が終わりました．\nピーク値は %d\nその時のフレーム番号は %d"%(peak_force, peak_flame))
+    # messagebox.showinfo("status", "ピーク粘着力の計算が終わりました．\nピーク値は %d\nその時のフレーム番号は %d"%(peak_force, peak_flame))
 
 
 # GUIrootの作成
@@ -173,7 +186,7 @@ figure_button = ttk.Button(frame2, text='Generate Figures', command = ClickedFig
 figure_button.pack(side=LEFT)
 
 # Peak Valueボタンの作成
-peak_button = ttk.Button(frame2, text='Calc Peak Value', command=PeakAdhesiveForce)
+peak_button = ttk.Button(frame2, text='Calc Peak Value', command=ClickedPeakButton)
 peak_button.pack(side=LEFT)
 
 # Cancelボタンの作成
